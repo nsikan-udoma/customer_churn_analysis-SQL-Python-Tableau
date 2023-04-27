57793d8dd0e6f83f08191153a4f7496d214d6218
nsikan-udoma <nsikan.e.udoma@gmail.com>
2023-04-27 11:43:28 -0400

    Updated notebook to include database integration step

---
+++ Supporting Files/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+PGHOST = 'mydbinstance.cocsbaafafpr.us-east-2.rds.amazonaws.com'
+
+PGDATABASE = 'dollarbankdb'
+
+PGUSER = 'postgres'
+
+PGPASSWORD = '3082nakisN!'
+
+PGPORT = '5432'
+
